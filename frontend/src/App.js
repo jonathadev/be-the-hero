f374c5fc7d61a16794543c65647558d3abba698c
JÔNATHA <jonatha.mendonca@hotmail.com>
2020-04-16 17:09:57 -0300

    Front-end do app

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './global.css';
+
+//import Header from './Header';
+import Routes from './routes';
+
+//JSX (JavaScript XML) Que é syntaxe do HTML
+
+// setState retorna Array {valor, funcaoDeAtualizacao}
+//  console.log(counter);
+function App() {
+
+  return (
+    <Routes />
+    );
+}
+
+export default App;
